5f59117257dd60d80a26d28af9f8a9d7512d3ebe
habib-abdillah <muhabibabd@gmail.com>
2025-08-20 09:26:42 +0700

    First commit

---
+++ database/migrations/2025_08_20_015405_create_barangs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('barangs', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_barang');
+            $table->string('kode_inventaris');
+            $table->integer('kategori_id');
+            $table->integer('ruangan_id');
+            $table->integer('tahun_pengadaan');
+            $table->string('sumber_dana');
+            $table->string('kondisii');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('barangs');
+    }
+};
